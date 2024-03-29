0f477f2e3efb4cfb902629b3c9975f8a0b368374
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2024-03-30 04:38:44 +0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<header class="bg-[#F7F6F4] h-screen">
+		<div class="container flex justify-center items-center pt-5 gap-5">
+			<div>
+				<el-input
+					v-model="input"
+					style="width: 240px"
+					placeholder="User Nick"
+				/>
+			</div>
+
+			<el-button type="danger">Search</el-button>
+		</div>
+	</header>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+let input = ref('');
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './assets/main.css';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.use(ElementPlus);
+app.use(createPinia());
+
+app.mount('#app');
